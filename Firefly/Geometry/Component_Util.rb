36def963abe042c60d78976f85590358dd762808
aartools <hermasetas@gmail.com>
2022-09-06 13:58:01 +0200

    Deprecated functions removed

--- Firefly/Geometry/Component_Util.rb
+++ Firefly/Geometry/Component_Util.rb
@@ -1,32 +1,7 @@
 module Firefly
   # A module with handy component methods
   module ComponentUtil
-    # DEPRECATED
-    def self.calc_all_definition_levels(definitions)
-      levels = Hash.new(0)
-
-      definitions.each do |defi|
-        next if defi.count_instances.zero?
-
-        current_level = 0
-
-        calc_definition_levels(defi, levels, current_level)
-      end
-      levels
-    end
-
-    # DEPRECATED
-    def self.calc_definition_levels(defi, levels, current_level)
-      levels[defi] = [levels[defi], current_level].max
-
-      defi.entities.each do |e|
-        case e
-        when Sketchup::Group
-          calc_definition_levels(e, levels, current_level + 1)
-        when Sketchup::ComponentInstance
-          calc_definition_levels(e.definition, levels, current_level + 1)
-        end
-      end
-    end
+    
+    
   end
 end
