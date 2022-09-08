b2d47586d44356ff8be6b0c3f9d7751f3c4b1af6
aartools <hermasetas@gmail.com>
2022-09-08 10:33:49 +0200

    Image result dialog added

--- Firefly/UI/Dialog.rb
+++ Firefly/UI/Dialog.rb
@@ -2,8 +2,10 @@ module Firefly
   module Dialog
     def self.init_ui
       show_viewbased_window = UI::Command.new('Show View-based dialog') { ViewBased.show_dialog }
-      show_viewbased_window.menu_text = 'Show View-based dialog'
       UI.menu('Extensions').add_item show_viewbased_window
+
+      show_imageresult_window = UI::Command.new('Show Image Result dialog') { ImageResult.show_dialog }
+      UI.menu('Extensions').add_item show_imageresult_window
     end
   end
 end
