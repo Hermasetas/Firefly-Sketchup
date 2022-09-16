bb685b5fd2a37a82ef85f12b10b7eca3978d244e
aartools <hermasetas@gmail.com>
2022-09-16 13:07:57 +0200

    Firefly edited

--- Firefly.rb
+++ Firefly.rb
@@ -1,12 +1,4 @@
 module Firefly
-  unless file_loaded?(__FILE__)
-    ex = SketchupExtension.new('Firefly', 'Firefly/Main.rb')
-    ex.description = 'Firefly Daylight and Lighting simulations.'
-    ex.creator = 'Hermasetas'
-    Sketchup.register_extension(ex, true)
-    file_loaded(__FILE__)
-  end
-
   def self.reload
     files = Dir['C:/Users/herma/Documents/GitHub/Firefly-Sketchup/Firefly/**/*.rb']
     files.each do |f|
@@ -14,5 +6,20 @@ module Firefly
       puts "#{File.basename(f)} loaded"
     end
     nil
+  end
+
+  def self.extension
+    Sketchup.extensions['Firefly']
+  end
+
+  unless file_loaded?(__FILE__)
+    ex = SketchupExtension.new('Firefly', 'Firefly/Main.rb')
+    ex.description = 'Firefly Daylight and Lighting simulations.'
+    ex.creator = 'Hermasetas'
+    Sketchup.register_extension(ex, true)
+    file_loaded(__FILE__)
+
+    reload
+    Dialog.init_ui
   end
 end
