cf4bdd931925dd3fba23e80cdc5f108cd89cc25b
aartools <hermasetas@gmail.com>
2022-09-20 22:02:23 +0200

    Sky_options done

--- Firefly/Attributes.rb
+++ Firefly/Attributes.rb
@@ -1,15 +1,19 @@
+require JSON
+
 module Firefly
   module Attributes
-
     def self.save_sky_options(sky_options)
       model = Sketchup.active_model
-      model.set_attribute 'Firefly', 'sky_options', sky_options
+      model.set_attribute 'Firefly', 'sky_options', sky_options.to_json
     end
 
     def self.sky_options
       model = Sketchup.active_model
-      model.get_attribute 'Firefly', 'sky_options', ''
-    end
+      sky_options = model.get_attribute 'Firefly', 'sky_options'
 
+      return nil if sky_options.nil?
+
+      JSON.parse sky_options
+    end
   end
 end
