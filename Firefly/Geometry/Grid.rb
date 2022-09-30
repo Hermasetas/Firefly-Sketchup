39b6b277cf5d4e56d1a3a3f72252792626b40867
aartools <hermasetas@gmail.com>
2022-09-30 13:52:05 +0200

    DF simulation working

--- Firefly/Geometry/Grid.rb
+++ Firefly/Geometry/Grid.rb
@@ -6,31 +6,31 @@ module Firefly
     # @param spacing the space between points in meters
     # @param height The height to offset the points above the face
     # @return A 2D array of points
-    def self.create_grid(face, spacing, height = 0)
+    def self.create_points(face, spacing, height = 0)
       bounds = face.bounds
       x = bounds.min.x
       y = bounds.min.y
       z = bounds.min.z
       w = bounds.width
       h = bounds.height
-
+      
       # Meters to inches
       spacing = spacing.m
       height = height.m
-
+      
       nx = (w / spacing).ceil
       ny = (h / spacing).ceil
-
+      
       pts = []
-
+      
       for i in (0..nx-1) do
         pts << []
-
+        
         for j in (0..ny-1) do
           px = x + spacing * i
           py = y + spacing * j
           p = [px, py, z]
-
+          
           if face.classify_point(p) != Sketchup::Face::PointOutside
             p.x = p.x.round(2)
             p.y = p.y.round(2)
@@ -41,34 +41,39 @@ module Firefly
           end
         end
       end
-
+      
       pts
     end
-
-    def self.create_cpoints(pts)
+    
+    # Creates a group in Sketchup conatining the grid points and other information.
+    # @param pts A 2D array of points.
+    # @return A Sketchup::Group of cpoints
+    def self.create_grid_group(pts, name)
       Sketchup.active_model.start_operation('Create grid points', true)
-
+      
       ents = Sketchup.active_model.active_entities
-      g = ents.add_group.entities
-
-      pts.each do |l|
-        l.each do |p|
-          g.add_cpoint(p) if p
+      g = ents.add_group
+      g.name = name
+      g.set_attribute 'Firefly', 'isGrid', true
+      ents = g.entities
+      
+      pts.each do |row|
+        row.each do |p|
+          ents.add_cpoint p if p
         end
       end
-
+      
       Sketchup.active_model.commit_operation
     end
-
-    def self.grid_to_file(pts, file_name)
+    
+    # Writes a firefly grid to a text file with a direction vector of [0,0,1].
+    # @param grid A Sketchup::Group containing a series of cpoints
+    # @param file_name The name of the text file to write to
+    def self.grid_to_file(grid, file_name)
       File.open(file_name, 'w') do |file|
-        for l in pts
-          for p in l
-            if p
-              pm = MathUtil.convert_point_to_metric p
-              file.puts "#{pm.x} #{pm.y} #{pm.z}"
-            end
-          end
+        grid.entities.each do |p|
+          pm = MathUtil.convert_point_to_metric p.position
+          file.puts "#{pm.x} #{pm.y} #{pm.z} 0 0 1"
         end
       end
     end
--- Firefly/Radiance/Grid Based Simulation/Daylight_Factor.rb
+++ Firefly/Radiance/Grid Based Simulation/Daylight_Factor.rb
@@ -4,14 +4,14 @@ module Firefly
       Directory.clear_working_dir
       working_dir = Directory.working_dir
 
-      materials_file, faces_file, instances_file = SkpToRad.write_all_to_rad(dir_name)
+      materials_file, faces_file, instances_file = SkpToRad.write_all_to_rad(working_dir)
       sky_file = File.join(working_dir, 'sky.rad')
       RadSky.generate_daylight_factor_sky sky_file
 
       grid_file = File.join(working_dir, 'grid.inp')
       Grid.grid_to_file(grid, grid_file)
 
-      rtrace_params = Options.rtace_params options['params_label']
+      rtrace_params = Options.rtrace_params 'Fast'#options['params_label']
       command_file = File.join(working_dir, 'command_file.bat')
 
       t = Time.now.to_s.gsub(':', '-')[0..18]
@@ -20,9 +20,9 @@ module Firefly
 
       File.open(command_file, 'w') do |file|
         file.puts "cd /D \"#{working_dir}\""
-        file.puts "oconv \"#{materials_file}\" \"#{faces_file}\" \"#{instances_file}\" #{sky_file}> scene.oct"
-        file.puts "rtrace -I #{rtrace_params} scene.oct < #{grid_file} ^"
-        file.puts "| rcalc -e \"$1=0.265*$1 + 0.670*$2 + 0.065*$3\" > #{result_file}"
+        file.puts "oconv \"#{materials_file}\" \"#{faces_file}\" \"#{instances_file}\" \"#{sky_file}\"> scene.oct"
+        file.puts "rtrace -I -h #{rtrace_params} scene.oct < \"#{grid_file}\" ^"
+        file.puts "| rcalc -e \"$1=0.265*$1 + 0.670*$2 + 0.065*$3\" > \"#{result_file}\""
       end
 
       # Await result
