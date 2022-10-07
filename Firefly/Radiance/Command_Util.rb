a0bc43fe71ddc17a203271a1456f0dac1a5ad249
aartools <hermasetas@gmail.com>
2022-10-07 11:32:35 +0200

    file name changed

--- Firefly/Radiance/Command_Util.rb
+++ Firefly/Radiance/Command_Util.rb
@@ -14,20 +14,20 @@ module Firefly
       UI.openURL("file://#{command_file}")
     end
 
-    # Replaces a specified keyword in a file with the contents of another file
+    def self.run(command_file)
+      UI.openURL("file://#{command_file}")
+    end
+
+    # Creates a command to replace a specified keyword in a file with the contents of another file
     # @param base_file The file to insert text into
     # @param content_file The file which contents to insert into the base_file
     # @param result_file The path where the result should be saved
     # @param keyword The keyword in base_file to replace with the contents of content_file
     # @return A string representing a powershell command
     def self.replace_command(base_file, content_file, result_file, keyword)
-      <<~command
-        powershell -Command `
-        "(gc -encoding Utf8 '#{base_file}') `
-        -replace '#{keyword}', `
-        (gc -encoding Utf8 -raw '#{content_file}') |`
-        Out-File -encoding Utf8 #{result_file}"
-      command
+      <<~COMMAND
+        powershell -Command "(gc -encoding Utf8 '#{base_file}') -replace '#{keyword}', (gc -encoding Utf8 -raw '#{content_file}') | Out-File -encoding Utf8 '#{result_file}'"
+      COMMAND
     end
   end
 end
